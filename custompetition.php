b6cde24dc38463cadfff0cb70803ad92d47115ab
Edsel <edsel.lopez@jmaconsulting.biz>
2019-03-28 12:06:40 +0530

    Added customizations for civipetition

---
+++ custompetition.php
@@ -0,0 +1,122 @@
+<?php
+define('SURVEY_ID', 2);
+
+require_once 'custompetition.civix.php';
+
+/**
+ * Implementation of hook_civicrm_config
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function custompetition_civicrm_config(&$config) {
+  _custompetition_civix_civicrm_config($config);
+}
+
+/**
+ * Implementation of hook_civicrm_xmlMenu
+ *
+ * @param $files array(string)
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function custompetition_civicrm_xmlMenu(&$files) {
+  _custompetition_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implementation of hook_civicrm_install
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function custompetition_civicrm_install() {
+  _custompetition_civix_civicrm_install();
+}
+
+/**
+ * Implementation of hook_civicrm_uninstall
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function custompetition_civicrm_uninstall() {
+  _custompetition_civix_civicrm_uninstall();
+}
+
+/**
+ * Implementation of hook_civicrm_enable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function custompetition_civicrm_enable() {
+  _custompetition_civix_civicrm_enable();
+}
+
+/**
+ * Implementation of hook_civicrm_disable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function custompetition_civicrm_disable() {
+  _custompetition_civix_civicrm_disable();
+}
+
+/**
+ * Implementation of hook_civicrm_upgrade
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *                for 'enqueue', returns void
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function custompetition_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _custompetition_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implementation of hook_civicrm_managed
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function custompetition_civicrm_managed(&$entities) {
+  _custompetition_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implementation of hook_civicrm_caseTypes
+ *
+ * Generate a list of case-types
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function custompetition_civicrm_caseTypes(&$caseTypes) {
+  _custompetition_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implementation of hook_civicrm_alterSettingsFolders
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function custompetition_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _custompetition_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * Implements hook_civicrm_postProcess().
+ *
+ * @param string $formName
+ * @param CRM_Core_Form $form
+ */
+function custompetition_civicrm_postProcess($formName, &$form) {
+  if ($formName == "CRM_Campaign_Form_Petition_Signature" && $form->_surveyId == SURVEY_ID) {
+    $url = "https://www.environmentalhealth.org/index.php/en/thank-you-for-taking-action-on-ab-423";
+    CRM_Utils_System::redirect($url);
+  }
+}
